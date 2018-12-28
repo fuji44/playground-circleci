a3d07b14657ad7900ad42ea7aa8c43fd00be3881
Yoshihiro Fujimoto <yoshihiro-fujimoto@bm-sms.co.jp>
2018-12-28 20:02:35 +0900

    mavenのdummyプロジェクトコードを追加

---
+++ src/main/java/com/fuji44/App.java
@@ -0,0 +1,13 @@
+package com.fuji44;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
